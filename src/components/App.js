a7f62a9020e6482b10ec0b622f877277944cc170
unknown <chanhuicom@gmail.com>
2020-10-01 23:53:59 +0900

    폰트 변경 및 패턴 디자인 적용

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import {HashRouter as Router, Route} from "react-router-dom"
 import Home from "../routes/Home"
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+`;
 
 function App() {
   return (
+    <>
+     <GlobalStyles/>
     <Router>
       <Route exact path="/" component={Home} />
     </Router>
+    </>
   );
 }
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 import styled from "styled-components"
+import Bounce from 'react-reveal/Bounce';
+import Fade from 'react-reveal/Fade';
 
 const Container = styled.div`
   display: flex;
@@ -10,7 +12,11 @@ const Container = styled.div`
   width: 100%;
 `;
 const Header = styled.header`
-  background-image: linear-gradient(-40deg, #0E5FCC, #37B0E4);
+  background-color: #2F9FE7 ;
+  background-image: linear-gradient(45deg, #0896E7 25%, transparent 25%, transparent 75%, #0896E7 75%, #0896E7), linear-gradient(45deg, #0896E7 25%, transparent 25%, transparent 75%, #0896E7 75%, #0896E7);
+  background-position: 0 0, 25px 25px;
+  background-size: 50px 50px;
+
   height: 95vh;
   color: white;
   display: flex;
@@ -21,19 +27,29 @@ const Header = styled.header`
 `;
 const Dao = styled.img`
   position:absolute;
+  max-width:80%;
   top:0;
   left:0;
 `;
 const Dizni = styled.img`
   position:absolute;
+  max-width:80%;
   top:0;
   right:0;
 `;
 
+const TitleImg = styled.img`
+  margin-top:-350px;
+  max-width:100%;
+`;
+
 const Title = styled.h1`
-  font-size: 60px;
-  font-weight: 600;
+  font-size: 20px;
+  font-weight: 500;
   margin-bottom: 20px;
+  background-color: rgba(0,0,0, 0.8);
+  padding: 1vh;
+  border-radius: 10px;
 `;
 const Subtitle = styled.h3`
   font-size: 35px;
@@ -50,13 +66,19 @@ export default () => {
   //const { loading, data } = useQuery(GET_MOVIES);
   return(
     <>
-    <Dao src={'/cha1.png'}></Dao>
-    <Dizni src={'/cha4.png'}></Dizni>
-
+    <Bounce top>
+      <Dao src={'/cha1.png'}></Dao>
+      <Dizni src={'/cha4.png'}></Dizni>
+    </Bounce>
     <Container>     
-      <Header>
-        <Title>Apollo 2020</Title>
+      <Header>  
+      <Bounce top>
+        <TitleImg src={'/title.png'}></TitleImg>
+        </Bounce>
+        <Fade top>
+        <Title>KartRider 오픈 API로 제작한 유저정보 조회 페이지입니다</Title>
         <Subtitle>I love GraphQL</Subtitle>
+        </Fade>
       </Header>   
     </Container>
     </>
