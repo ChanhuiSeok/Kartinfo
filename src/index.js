3c8b2d9ac3fbb0ff9f283daab0dbc7abfd3a3064
unknown <chanhuicom@gmail.com>
2020-10-05 16:31:06 +0900

    heroku : reset css 파일 수정

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App'
 import { ApolloProvider } from "@apollo/react-hooks";
 import client from "./apollo";
+import './reset.css';
 
 ReactDOM.render(
   <ApolloProvider client={client}>
