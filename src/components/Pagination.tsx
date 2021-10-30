55e0731ebcd459d0476115b6884ddb2ce3c44458
unknown <chanhuicom@gmail.com>
2021-10-31 01:38:50 +0900

    안쓰는 import 삭제

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, FunctionComponent } from "react";
+import React, { FunctionComponent } from "react";
 import styled from "styled-components";
 import { getPageNums } from "./util";
 
